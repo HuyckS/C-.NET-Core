78d3dd662a880f3b8be7d0944e8b77265cde5f96
Spencer Huyck <huycksl@gmail.com>
2021-02-19 15:22:07 -0800

    Feb 19 Updates

--- assignments/LoginAndReg/Controllers/HomeController.cs
+++ assignments/LoginAndReg/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Http;
 using LoginAndReg.Models;
 
 namespace LoginAndReg.Controllers
@@ -25,21 +26,90 @@ namespace LoginAndReg.Controllers
             return View();
         }
 
-        [HttpPost("/create-user")]
+        [HttpPost("create-user")]
         public IActionResult CreateUser(User user)
         {
             if (ModelState.IsValid)
             {
+                if (_context.Users.Any(u => u.Email == user.Email))
+                {
+                    ModelState.AddModelError("Email", "Email is already in use -- please register with a different email.");
+                    return View("Index");
+                }
                 PasswordHasher<User> Hasher = new PasswordHasher<User>();
                 user.Password = Hasher.HashPassword(user, user.Password);
                 _context.Users.Add(user);
                 _context.SaveChanges();
+                HttpContext.Session.SetInt32("UserId", user.UserId);
                 return RedirectToAction("Success");
             }
             else
             {
                 return View("Index");
             }
+        }
+
+        [HttpGet("login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost("login-user")]
+        public IActionResult LoginUser(LoginUser userSubmission)
+        {
+            if (ModelState.IsValid)
+            {
+                var userInDb = _context.Users
+                    .FirstOrDefault(u => u.Email == userSubmission.Email);
+
+                if (userInDb == null)
+                {
+                    ModelState.AddModelError("Email", "Invalid Email/Password");
+                    return View("Login");
+                }
+
+                var hasher = new PasswordHasher<LoginUser>();
+                var result = hasher.VerifyHashedPassword(userSubmission, userInDb.Password, userSubmission.Password);
+
+                if (result == 0)
+                {
+                    ModelState.AddModelError("Password", "Invalid Email/Password");
+                    return View("Login");
+                }
+                else
+                {
+                    //store id in session?
+                    HttpContext.Session.SetInt32("UserId", userInDb.UserId);
+                    return RedirectToAction("SignedIn");
+                }
+            }
+            return View("Login");
+        }
+
+        [HttpGet("signedIn")]
+        public IActionResult SignedIn()
+        {
+            var signedIn = HttpContext.Session.GetInt32("UserId");
+            if (signedIn != null)
+            {
+                return RedirectToAction("Success");
+            }
+
+            return RedirectToAction("Login");
+        }
+
+        [HttpGet("success")]
+        public IActionResult Success()
+        {
+            return View();
+        }
+
+        [HttpGet("logout")]
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return View("Login");
         }
 
         public IActionResult Privacy()
